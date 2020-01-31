abff3c620eebc9c0e62c902e05c34464352b2a0b
Mikita Kukavenka <nkukovenko@by.byelex.com>
2020-01-31 17:19:22 +0300

    added

---
+++ src/Words.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Words where
+
+import qualified Data.Text                     as T
+
+type TranslationComments = [T.Text]
+type Word = Data.Text
+type Words = [Word]
+-- SoundLaw = ?
+type SoundLaws = [SoundLaw]
+
+migrateWords :: Words -> SoundLaws -> Words
+migrateWord :: Word -> SoundLaws -> Word
+
+
+
+
+
