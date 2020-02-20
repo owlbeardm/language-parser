05cfc8a8eaffe0ad03b91b3dc1e262b20c8a86c3
Mikita Kukavenka <nkukovenko@by.byelex.com>
2020-02-20 18:48:23 +0300

    Added phonetic changes

--- src/PhoneticChanges.hs
+++ src/PhoneticChanges.hs
@@ -1,34 +1,57 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module PhoneticChanges where
+module PhoneticChanges
+  (
+  ) where
 
-import qualified Data.Text                     as T
+import qualified Data.Text  as T
+import qualified Text.Regex as R
 
 type PhoneticWord = T.Text
 type Translation = T.Text
 type WrittenWord = T.Text
-type WritingSystem = T.Text
-data SoundLaw = SoundLaw T.Regex T.Text
 
--- PhoneticWord → Translation
-translate :: PhoneticWord -> Translation
+data SoundLaw =
+  SoundLaw R.Regex T.Text
 
--- Writing system →   Phonetic Word → Written Word
-getWritten :: WritingSystem -> PhoneticWord -> WrittenWord
+data WritingLaw =
+  WritingLaw R.Regex T.Text
 
+type WritingSystem = [WritingLaw]
 
--- Sound law -> Phonetic Word → Phonetic Word
-changeWord :: SoundLaw -> PhoneticWord -> PhoneticWord
+-- -- PhoneticWord → Translation
+-- translate :: PhoneticWord -> Translation
+-- -- Writing system →   Phonetic Word → Written Word
+-- getWritten :: WritingSystem -> PhoneticWord -> WrittenWord
+changeWord :: PhoneticWord -> SoundLaw -> PhoneticWord
+changeWord word (SoundLaw regex changedPart) =
+  T.pack (R.subRegex regex (T.unpack word) (T.unpack changedPart))
 
--- Sound laws -> Phonetic Word → Phonetic Word
-migrateWord :: [SoundLaw] -> PhoneticWord -> PhoneticWord
+migrateWord :: PhoneticWord -> [SoundLaw] -> PhoneticWord
+migrateWord word soundLaws = foldl changeWord word soundLaws
 
--- Sound laws -> Phonetic Words → Phonetic Words
-migrateWords :: [SoundLaw] -> [PhoneticWord] -> [PhoneticWord]
+migrateWords :: [PhoneticWord] -> [SoundLaw] -> [PhoneticWord]
+migrateWords words laws = map ((flip migrateWord) laws) words
 
+who = T.pack "kʷei"
 
--- ////////
--- Prelude Text.Regex> :t subRegex 
--- subRegex :: Regex -> String -> String -> String
--- /////////
--- Prelude Text.Regex> subRegex (mkRegex "foo") "foobar" "123" "123bar"
+nine = T.pack "H1néwn̥"
+
+firstRegex = R.mkRegex "ei"
+
+secondRegex = R.mkRegex "^H1"
+
+thirdRegex = R.mkRegex "H1"
+
+firstSl = SoundLaw firstRegex "ē"
+
+secondSl = SoundLaw secondRegex "a"
+
+thirdSl = SoundLaw thirdRegex ""
+
+listSl = [firstSl, secondSl, thirdSl]
+
+listW = [who, nine]
+-- ei      > ē
+-- H1#     > a
+-- H1      >
--- src/Words.hs
+++ src/Words.hs
@@ -4,14 +4,14 @@ module Words where
 
 import qualified Data.Text                     as T
 
-type TranslationComments = [T.Text]
-type Word = Data.Text
-type Words = [Word]
--- SoundLaw = ?
-type SoundLaws = [SoundLaw]
+-- type TranslationComments = [T.Text]
+-- type Word = Data.Text
+-- type Words = [Word]
+-- -- SoundLaw = ?
+-- type SoundLaws = [SoundLaw]
 
-migrateWords :: Words -> SoundLaws -> Words
-migrateWord :: Word -> SoundLaws -> Word
+-- migrateWords :: Words -> SoundLaws -> Words
+-- migrateWord :: Word -> SoundLaws -> Word
 
 
 
