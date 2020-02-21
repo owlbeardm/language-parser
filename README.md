3cee9ecd3e95d221c14bb5c86f3e00f9755266e2
Mikita Kukavenka <nkukovenko@by.byelex.com>
2020-02-21 20:24:20 +0300

    added files read

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # language-parser
 
 https://github.com/commercialhaskell/stack/blob/master/doc/GUIDE.md
+
+
+
+## to print unicode in console
+
+:set -interactive-print=Print.UnescapingPrint.uprint
--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,18 @@ module Main where
 import           Lib
 import qualified PhoneticChanges  as PC
 import           System.Directory
+import           System.IO
+
+-- main :: IO ()
+-- main = do
+--     -- userInput <- getContents
+--     createDirectory "userInput"
+--     putStrLn "done!"
 
 main :: IO ()
 main = do
-    -- userInput <- getContents
-    createDirectory "userInput"
-    putStrLn "done!"
+--  myFile <- openFile "langs/proto material/initial_words" ReadMode
+ fileName <- getLine
+ putStrLn (fileName ++ " words list")
+ input <- readFile ("langs/" ++ fileName ++ "/initial_words")
+ putStrLn (foldr (++) "." ((map (++ ", ") . lines) input))
