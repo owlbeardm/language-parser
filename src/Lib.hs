b155284565957315c6b4d6ac19fd4b730b9d3013
Mikita Kukavenka <nkukovenko@by.byelex.com>
2020-03-02 18:07:11 +0300

    sqlite example

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,10 +2,54 @@ module Lib
   ( libMain
   ) where
 
--- import           Control.Applicative
--- import           Data.Time
+import           Control.Applicative
+import           Data.Time
 -- import           Database.PostgreSQL.Simple
 -- import           Database.PostgreSQL.Simple.FromRow
 
 libMain :: IO ()
-libMain = print "db-lesson"
+libMain = print ['a', 'b']
+
+data Language = Language
+ {  langId            :: Int
+    , langVersion     :: Int
+    , langCreatedby   :: String
+    , langCreatedwhen :: Day
+    , langModiby      :: String
+    , langModiwhen    :: Day
+    , langName        :: String
+ }
+
+instance Show Language where
+    show lang = mconcat [ show $ langId lang
+      , ".) "
+      , langName lang]
+
+data LangWord = LangWord
+ {   wordId           :: Int
+    , wordVersion     :: Int
+    , wordCreatedby   :: String
+    , wordCreatedwhen :: Day
+    , wordModiby      :: String
+    , wordModiwhen    :: Day
+    , wordText        :: String
+ }
+
+instance Show LangWord where
+    show word = mconcat [ show $ wordId word
+      , ".) "
+      , wordText word]
+
+addWord :: String -> IO ()
+addWord word = do
+ conn <- open "tools.db"
+ execute conn "INSERT INTO word_tbl (word) VALUES (?)"
+ (Only word)
+ print "word added"
+ close conn
+
+withConn :: String -> (Connection -> IO ()) -> IO ()
+withConn dbName action = do
+ conn <- open dbName
+ action conn
+ close conn
