7d3d0654cb6edd80027e9a2a0a518d43b5f70b6f
c7d5a6 <godinfrog@gmail.com>
2020-03-04 02:07:35 +0300

    changes

---
+++ sql/testdata/Readme.md
@@ -0,0 +1,5 @@
+# run
+
+```
+psql --cluster 11/main -h 172.20.7.103 -p 5432 -d wiki -U wiki < filename.sql
+```
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,8 @@
+{-# LANGUAGE QuasiQuotes     #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE QuasiQuotes #-}
 
 module Lib
-  ( 
+  (
   ) where
 
 import           ClassyPrelude
@@ -12,20 +12,34 @@ import           Data.Time
 import           Database.PostgreSQL.Simple
 import           Database.PostgreSQL.Simple.FromRow
 
-mainPrint :: (Show a) => (Connection -> IO [a]) -> IO ()
-mainPrint action = do
+mainPrintL :: (Show a) => (Connection -> IO [a]) -> IO ()
+mainPrintL action = do
   pool <- acquirePool
-  a <- withConn pool action
+  a <- withResource pool action
   mapM_ print a
 
+mainPrintML :: (Show a) => (Connection -> IO (Maybe [a])) -> IO ()
+mainPrintML action = do
+  pool <- acquirePool
+  a <- withResource pool action
+  case a of
+    Just list -> mapM_ print list
+    _ -> print a
+
+mainPrintOne :: (Show a) => (Connection -> IO a) -> IO ()
+mainPrintOne action = do
+  pool <- acquirePool
+  a <- withResource pool action
+  print a
+
 data Language = Language
- {  langId            :: Int
-    , langVersion     :: Int
-    , langCreatedby   :: String
+ {  langId          :: Int
+    , langVersion   :: Int
+    , langCreatedby :: String
     -- , langCreatedwhen :: Day
-    , langModiby      :: String
+    , langModiby    :: String
     -- , langModiwhen    :: Day
-    , langName        :: String
+    , langName      :: String
  }
 
 instance Show Language where
@@ -66,6 +80,11 @@ getWordsByLangId langId = getByQuery qry (Only langId)
     qry = "SELECT id,version,createdby,modiby,word \
           \FROM word_tbl WHERE word_tbl.lang_id = (?) "
 
+getWordsByLangName :: String -> Connection -> IO (Maybe [LangWord])
+getWordsByLangName name conn = do
+  langId <- getLangIdByName name conn
+  mapM (`getWordsByLangId` conn) langId
+
 getLangs :: Connection -> IO [Language]
 getLangs = getByQuery "SELECT id,version,createdby,modiby,name FROM language_tbl" ()
 
@@ -73,16 +92,24 @@ getLangByName :: String -> Connection -> IO [Language]
 getLangByName langName = getByQuery qry (Only langName)
   where
     qry = "SELECT id,version,createdby,modiby,name \
-          \FROM language_tbl AS l WHERE l.name = (?) "
+          \FROM language_tbl AS l WHERE l.name = (?) \
+          \LIMIT 1"
+
+getLangIdByName :: String -> Connection -> IO (Maybe Int)
+getLangIdByName langName conn = do
+  results <- getLangByName langName conn
+  case results of
+    [lang] -> return $ Just (langId lang)
+    _ -> return Nothing
+
 
 acquirePool :: IO (Pool Connection)
-acquirePool = do
-  createPool (connect connInfo) close 1 10 10
+acquirePool = createPool (connect connInfo) close 1 10 10
   where
     connInfo = defaultConnectInfo {connectHost = "172.20.7.103", connectUser = "wiki", connectPassword = "wiki", connectDatabase = "wiki"}
 
 withConn :: Pool Connection -> (Connection -> IO a) -> IO a
-withConn pool action = withResource pool (\conn -> action conn)
+withConn  = withResource
 
 
 --   getAllWords :: Connection -> IO [LangWord]
