9c49d4f7498bad28d0ae0be10734b98c21acc68e
Mikita Kukavenka <nkukovenko@by.byelex.com>
2020-03-03 19:23:19 +0300

    added sql qry

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,22 +1,30 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Lib
-  ( libMain
+  ( 
   ) where
 
-import           Control.Applicative
+import           ClassyPrelude
+-- import           Control.Applicative
+import           Data.Pool
 import           Data.Time
--- import           Database.PostgreSQL.Simple
--- import           Database.PostgreSQL.Simple.FromRow
+import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.FromRow
 
-libMain :: IO ()
-libMain = print ['a', 'b']
+mainPrint :: (Show a) => (Connection -> IO [a]) -> IO ()
+mainPrint action = do
+  pool <- acquirePool
+  a <- withConn pool action
+  mapM_ print a
 
 data Language = Language
  {  langId            :: Int
     , langVersion     :: Int
     , langCreatedby   :: String
-    , langCreatedwhen :: Day
+    -- , langCreatedwhen :: Day
     , langModiby      :: String
-    , langModiwhen    :: Day
+    -- , langModiwhen    :: Day
     , langName        :: String
  }
 
@@ -25,14 +33,17 @@ instance Show Language where
       , ".) "
       , langName lang]
 
+instance FromRow Language where
+  fromRow = Language <$> field <*> field <*> field <*> field <*> field
+
 data LangWord = LangWord
- {   wordId           :: Int
-    , wordVersion     :: Int
-    , wordCreatedby   :: String
-    , wordCreatedwhen :: Day
-    , wordModiby      :: String
-    , wordModiwhen    :: Day
-    , wordText        :: String
+ {   wordId         :: Int
+    , wordVersion   :: Int
+    , wordCreatedby :: String
+    -- , wordCreatedwhen :: Day
+    , wordModiby    :: String
+    -- , wordModiwhen    :: Day
+    , wordText      :: String
  }
 
 instance Show LangWord where
@@ -40,16 +51,41 @@ instance Show LangWord where
       , ".) "
       , wordText word]
 
-addWord :: String -> IO ()
-addWord word = do
- conn <- open "tools.db"
- execute conn "INSERT INTO word_tbl (word) VALUES (?)"
- (Only word)
- print "word added"
- close conn
+instance FromRow LangWord where
+  fromRow = LangWord <$> field <*> field <*> field <*> field <*> field
 
-withConn :: String -> (Connection -> IO ()) -> IO ()
-withConn dbName action = do
- conn <- open dbName
- action conn
- close conn
+getByQuery :: (FromRow r, ToRow q) => Query -> q -> Connection -> IO [r]
+getByQuery qry params conn = query conn qry params
+
+getWords :: Connection -> IO [LangWord]
+getWords = getByQuery "SELECT id,version,createdby,modiby,word FROM word_tbl" ()
+
+getWordsByLangId :: Int -> Connection -> IO [LangWord]
+getWordsByLangId langId = getByQuery qry (Only langId)
+  where
+    qry = "SELECT id,version,createdby,modiby,word \
+          \FROM word_tbl WHERE word_tbl.lang_id = (?) "
+
+getLangs :: Connection -> IO [Language]
+getLangs = getByQuery "SELECT id,version,createdby,modiby,name FROM language_tbl" ()
+
+getLangByName :: String -> Connection -> IO [Language]
+getLangByName langName = getByQuery qry (Only langName)
+  where
+    qry = "SELECT id,version,createdby,modiby,name \
+          \FROM language_tbl AS l WHERE l.name = (?) "
+
+acquirePool :: IO (Pool Connection)
+acquirePool = do
+  createPool (connect connInfo) close 1 10 10
+  where
+    connInfo = defaultConnectInfo {connectHost = "172.20.7.103", connectUser = "wiki", connectPassword = "wiki", connectDatabase = "wiki"}
+
+withConn :: Pool Connection -> (Connection -> IO a) -> IO a
+withConn pool action = withResource pool (\conn -> action conn)
+
+
+--   getAllWords :: Connection -> IO [LangWord]
+-- getAllWords pool = do
+--   a <- withResource pool (\conn -> query_ conn "SELECT id,version,createdby,modiby,word FROM word_tbl" :: IO [LangWord])
+--   mapM_ print a
