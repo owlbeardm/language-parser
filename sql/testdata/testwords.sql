e906a6998dbfba291451a7561b9a8d6d255ffbad
Mikita Kukavenka <nkukovenko@by.byelex.com>
2020-03-03 19:31:03 +0300

    changes

--- sql/testdata/testwords.sql
+++ sql/testdata/testwords.sql
@@ -5,3 +5,6 @@ INSERT INTO language_tbl(id, version, name, createdby, createdwhen, modiby, modi
 INSERT INTO word_tbl(id, version, word, lang_id, createdby, createdwhen, modiby, modiwhen) VALUES
  (1, 1, 'H2enH1', 1, 'wiki', now(), 'wiki', now()),
  (2, 1, 'ĝonH2dʰos', 1, 'wiki', now(), 'wiki', now());
+
+
+ nextval('pk_sequence')
