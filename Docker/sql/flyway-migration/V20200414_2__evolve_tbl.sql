bc7286e183e0bf757664388fee7362218672e52e
Mikita Kukavenka <nkukovenko@by.byelex.com>
2020-04-15 15:30:26 +0300

    fixed sql

--- Docker/sql/flyway-migration/V20200414_2__evolve_tbl.sql
+++ Docker/sql/flyway-migration/V20200414_2__evolve_tbl.sql
@@ -3,10 +3,10 @@ CREATE TABLE evolve_law_tbl (
     lang_from_id bigint NOT NULL,
     lang_to_id bigint NOT NULL,
     priority bigint DEFAULT 0 NOT NULL,
-    soundRegexFrom text NOT NULL,
-    soundTo text NOT NULL,
+    sound_regex_from text NOT NULL,
+    sound_to text NOT NULL,
     CONSTRAINT evolve_law_pk PRIMARY KEY (id),
     CONSTRAINT evolve_law_tbl_lang_from_id_fkey FOREIGN KEY (lang_from_id) REFERENCES language_tbl(id),
     CONSTRAINT evolve_law_tbl_lang_to_id_fkey FOREIGN KEY (lang_to_id) REFERENCES language_tbl(id),
-    CONSTRAINT evolve_law_lang_from_id_lang_to_id_sound_law_id_unq UNIQUE (lang_from_id,lang_to_id,sound_law_id),
+    CONSTRAINT evolve_law_lang_from_id_lang_to_id_sound_regex_from_sound_to_unq UNIQUE (lang_from_id,lang_to_id,sound_regex_from,sound_to)
 );
