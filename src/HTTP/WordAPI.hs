1251e416d9f62f6344f821d3be46a8ea6ce4f1dd
Mikita Kukavenka <godinfrog@gmail.com>
2020-12-23 17:36:35 +0300

    changed words api

--- src/HTTP/WordAPI.hs
+++ src/HTTP/WordAPI.hs
@@ -7,7 +7,7 @@ module HTTP.WordAPI
       wordServer
       ) where
 
-import           ClassyPrelude          (map, return, ($))
+import           ClassyPrelude          (Text, map, return, ($))
 import           Control.Monad.IO.Class (liftIO)
 import           Database.Base          (LanguageName (..), runSQLAction)
 import           Database.Entity        (Language, Word)
@@ -15,36 +15,36 @@ import           Database.Esqueleto     (entityVal)
 import           Database.Translation   (WordDescription, WordSource,
                                          WordTranslation,
                                          getFullWordDescription)
-import           Database.Word          (listWordsByLang)
+import           Database.Word          (findWordsByText, listWordsByLang)
 import           Servant.API
 import           Servant.Server
 
 type WordDescriptionAPI = (Word, [Language], [WordTranslation], [WordSource])
 
 type WordsApi = "words" :>
-  (   Get '[JSON] [WordDescriptionAPI]
---   :<|> "all" :> Get '[JSON] [Language]
+  (    Get '[JSON] [WordDescriptionAPI]
+  :<|> Capture "word" Text :> Get '[JSON] [WordDescriptionAPI]
   )
 
 wordServer :: Server WordsApi
-wordServer = fetchLanguagesHandler
---   :<|> fetchLanguagesHandler
+wordServer =
+       fetchWordsHandler
+  :<|> lookUpWordsHandler
 
-fetchLanguagesHandler ::  Handler [WordDescriptionAPI]
-fetchLanguagesHandler = do
+fetchWordsHandler ::  Handler [WordDescriptionAPI]
+fetchWordsHandler = do
   words <- liftIO $ runSQLAction $ listWordsByLang langName
   fullDescr <- liftIO $ runSQLAction $ getFullWordDescription words
   return (map toWordDescriptionAPI fullDescr)
   where
-    langName = Titan
+    langName = Queran
 
--- printWordsFrom :: LanguageName -> IO ()
--- printWordsFrom langName = runSQLAction $ do
---      words <- listWordsByLang langName
---      fullDescr <- getFullWordDescription words
---      liftIO $ mapM_ (putStrLn . tshowPretty prettyWordDescription) fullDescr
---      putStr "\n\tTotal: "
---      print $ length words
+lookUpWordsHandler :: Text -> Handler [WordDescriptionAPI]
+lookUpWordsHandler word = do
+  words <- liftIO $ runSQLAction $ findWordsByText word
+  fullDescr <- liftIO $ runSQLAction $ getFullWordDescription words
+  return (map toWordDescriptionAPI fullDescr)
+
 
 toWordDescriptionAPI :: WordDescription -> WordDescriptionAPI
 toWordDescriptionAPI (a, b, c, d) = (entityVal a, b, c, d)
