1d16b23a9145bfc57d0150ff65b47ed260944e4d
Mikita Kukavenka <godinfrog@gmail.com>
2021-02-01 20:54:49 +0300

    Added const clusters to api

--- src/HTTP/WordAPI.hs
+++ src/HTTP/WordAPI.hs
@@ -1,51 +1,77 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE TypeOperators  #-}
 
 module HTTP.WordAPI
-      (
-      WordsApi,
-      wordServer
-      ) where
+  ( WordsApi
+  , wordServer
+  ) where
 
-import           ClassyPrelude          (Bool (..), Int64, Maybe (..), MonadIO,
-                                         Text, map, maxBound, minBound, return,
-                                         ($), (.))
-import           Database.Base          (LanguageName (..), PartOfSpeech (..),
-                                         runDb)
-import           Database.Esqueleto     (entityKey, entityVal, fromSqlKey)
-import           Database.Translation   (getFullWordDescription)
-import           Database.Word          (addWordByLangNameF, deleteWordById,
-                                         findWordsByText,
-                                         findWordsByTextAndLang,
-                                         getByWordByLangName, listWordsByLang,
-                                         updateWordById)
+import           ClassyPrelude        (Bool (..), Bounded, Enum, Eq, Generic,
+                                       Int64, Maybe (..), MonadIO, Read, Show,
+                                       Text, map, maxBound, minBound, return,
+                                       ($), (.))
+import           Data.Aeson           (FromJSON, ToJSON)
+import           Data.Swagger         (ToParamSchema (..), ToSchema (..))
+import           Database.Base        (LanguageName (..), PartOfSpeech (..),
+                                       runDb)
+import           Database.Esqueleto   (entityKey, entityVal, fromSqlKey)
+import           Database.Translation (getFullWordDescription)
+import           Database.Word        (addWordByLangNameF, deleteWordById,
+                                       findWordsByText, findWordsByTextAndLang,
+                                       getByWordByLangName, listWordsByLang,
+                                       listWordsInfo, updateWordById)
 import           HTTP.Config
-import           HTTP.Utility           (AddWordJSON (..), WordDescriptionAPI,
-                                         WordJSON (..), checkadded,
-                                         convertWordDescriptionAPI,
-                                         convertWordToWordJson)
+import           HTTP.Utility         (AddWordJSON (..), WordDescriptionAPI,
+                                       WordJSON (..), checkadded,
+                                       convertWordDescriptionAPI,
+                                       convertWordToWordJson)
+import           Language.Word        (getWordsConstClusters,
+                                       getWordsConstLastClusters,
+                                       getWordsConstStartingClusters)
 import           Servant.API
 import           Servant.Server
+import           Web.HttpApiData      (FromHttpApiData, parseQueryParam,
+                                       readTextData)
 
-type WordsApi = "words" :>
-  (    "lang" :> Capture "lang" LanguageName :> Get '[JSON] [WordJSON]
-  :<|> ReqBody '[JSON] AddWordJSON :> Post '[JSON] Bool
-  :<|> Capture "wordId" Int64 :> Delete '[JSON] ()
-  :<|> Capture "wordId" Int64 :> ReqBody '[JSON] AddWordJSON :> Post '[JSON] ()
-  :<|> "pos" :> Get '[JSON] [PartOfSpeech]
-  :<|> Capture "word" Text :> QueryParam "lang" LanguageName :> Get '[JSON] [WordDescriptionAPI]
-  :<|> "exists" :> ReqBody '[JSON] AddWordJSON :>  Post '[JSON] Bool
-  )
+type WordsApi
+   = "words" :> ("lang" :> Capture "lang" LanguageName :> Get '[ JSON] [WordJSON] -- --
+                  :<|> ReqBody '[ JSON] AddWordJSON :> Post '[ JSON] Bool -- --
+                  :<|> Capture "wordId" Int64 :> Delete '[ JSON] () --
+                  :<|> Capture "wordId" Int64 :> ReqBody '[ JSON] AddWordJSON :> Post '[ JSON] () --
+                  :<|> "pos" :> Get '[ JSON] [PartOfSpeech] --
+                  :<|> Capture "word" Text :> QueryParam "lang" LanguageName :> Get '[ JSON] [WordDescriptionAPI] --
+                  :<|> "exists" :> ReqBody '[ JSON] AddWordJSON :> Post '[ JSON] Bool --
+                  :<|> "constclusters" :> Capture "lang" LanguageName :> QueryParam "clusterType" ClusterType :> Get '[ JSON] [Text] --
+                 ) --
+
+data ClusterType
+  = AllClusters
+  | StartingCluster
+  | LastClusters
+  deriving ( Eq
+           , Bounded
+           , Enum
+           , Read
+           , Show
+           , Generic
+           , ToJSON
+           , FromJSON
+           , ToSchema
+           , ToParamSchema
+           )
+
+instance FromHttpApiData ClusterType where
+  parseQueryParam = readTextData
 
 wordServer :: MonadIO m => ServerT WordsApi (AppServerT m)
 wordServer =
-       fetchWordsHandler
-  :<|> addWord
-  :<|> deleteWord
-  :<|> updateWord
-  :<|> fetchPosHandler
-  :<|> lookUpWordsHandler
-  :<|> lookUpWordExistsHandler
+  fetchWordsHandler :<|> addWord :<|> deleteWord :<|> updateWord :<|>
+  fetchPosHandler :<|>
+  lookUpWordsHandler :<|>
+  lookUpWordExistsHandler :<|>
+  fetchConstClusters
 
 fetchWordsHandler :: MonadIO m => LanguageName -> AppServerT m [WordJSON]
 fetchWordsHandler langName = do
@@ -67,9 +93,13 @@ updateWord :: MonadIO m => Int64 -> AddWordJSON -> AppServerT m ()
 updateWord wordId (AddWordJSON _ p w f) = runDb $ updateWordById wordId w p f
 
 fetchPosHandler :: MonadIO m => AppServerT m [PartOfSpeech]
-fetchPosHandler = return [minBound..maxBound]
+fetchPosHandler = return [minBound .. maxBound]
 
-lookUpWordsHandler :: MonadIO m => Text -> Maybe LanguageName -> AppServerT m [WordDescriptionAPI]
+lookUpWordsHandler ::
+     MonadIO m
+  => Text
+  -> Maybe LanguageName
+  -> AppServerT m [WordDescriptionAPI]
 lookUpWordsHandler wd mLang = do
   words <- runDb $ findWords wd mLang
   fullDescr <- runDb $ getFullWordDescription words
@@ -84,3 +114,14 @@ lookUpWordExistsHandler (AddWordJSON l p w _) = do
   case mw of
     Nothing -> return False
     _       -> return True
+
+fetchConstClusters ::
+     MonadIO m => LanguageName -> Maybe ClusterType -> AppServerT m [Text]
+fetchConstClusters langName mClstType = do
+  clstr <- runDb $ listWordsInfo langName (getClusterByType mClstType)
+  return clstr
+  where
+    getClusterByType Nothing                = getWordsConstClusters
+    getClusterByType (Just AllClusters)     = getWordsConstClusters
+    getClusterByType (Just StartingCluster) = getWordsConstStartingClusters
+    getClusterByType (Just LastClusters)    = getWordsConstLastClusters
